435bf5cf10a3fd5c1be9536444ad4cad65709138
Melissa Funk <melissafunk85@gmail.com>
2023-02-13 20:28:55 -0800

    add date btn

--- client/src/RecipeCard.js
+++ client/src/RecipeCard.js
@@ -12,9 +12,7 @@ function RecipeCard({ recipe }) {
     fetch(`/recipes/${recipe.id}`, {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        status: "made"
-      })
+      body: JSON.stringify({ status: "made" })
     })
     .then(res => res.json())
     .then(data => console.log(data))
@@ -24,9 +22,17 @@ function RecipeCard({ recipe }) {
     fetch(`/recipes/${recipe.id}`, {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        favorite: true
-      })
+      body: JSON.stringify({ favorite: true })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  const handleAddDateClick = (e) => {
+    fetch (`/recipes/${recipe.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ date: e.target.value })
     })
     .then(res => res.json())
     .then(data => console.log(data))
@@ -39,6 +45,17 @@ function RecipeCard({ recipe }) {
       {recipe.status === "new" ? <button onClick={handleMadeClick}>Made</button> : null}
       {recipe.favorite === null ? <button onClick={handleFavoriteClick}>Favorite</button> : null}
       <button onClick={handleDeleteClick}>Delete</button>
+
+      <select onChange={handleAddDateClick}>
+        <option>Select Date:</option>
+        <option value="Sun">Sunday</option>
+        <option value="Mon">Monday</option>
+        <option value="Tues">Tuesday</option>
+        <option value="Wed">Wednesday</option>
+        <option value="Thurs">Thursday</option>
+        <option value="Fri">Friday</option>
+        <option value="Sat">Saturday</option>
+      </select>
     </div>
   )
 }
